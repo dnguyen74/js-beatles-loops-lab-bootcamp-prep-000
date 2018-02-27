8c495ec1e56917df23ebd24ae5009e2a6f237c24
Dat Nguyen <dnguyen74@gmail.com>
2018-02-27 04:38:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function theBeatlesPlay(musicians, instruments) {
   var Beatles = {}
+    for (let i = 0; i < 25; i++) {
+	  if (i===1) {
+	    arrayS.push("I am 1 strange loop.");
+    }
+    else{
+      arrayS.push("I am ${i} strange loops.");
+    }
+  }
+
 }
