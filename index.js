7c32849018702195638e076b3e174ace692db70b
Dat Nguyen <dnguyen74@gmail.com>
2018-02-27 04:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
-  var Beatles = {}
+  var Beatles = {};
     for (let i = 0; i < 25; i++) {
-	  if (i===1) {
-	    arrayS.push("I am 1 strange loop.");
-    }
-    else{
-      arrayS.push("I am ${i} strange loops.");
+	    
+      Beatles.push(musicians[i] + " plays "+ instruments[i])"I am ${i} strange loops.");
     }
   }
 
